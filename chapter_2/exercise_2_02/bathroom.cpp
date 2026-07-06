2223790aabff700a4c9616708c7a0c1d9dd85e11
Christian Ruelas <christianruelas623@gmail.com>
2026-07-06 16:08:58 -0700

    Add PPP2 chapter 2 exercise 2.02

---
+++ chapter_2/exercise_2_02/bathroom.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Directions to upstairs bathroom at grandpa's house
+
+int main()
+{
+    std::cout << "1. Enter front door" << std::endl;
+    std::cout << "2. Go up the stairs" << std::endl;
+    std::cout << "3. Look left" << std::endl;
+    std::cout << "4. Walk slightly right and you should see the bathroom" << std::endl;
+ 
+    return 0;
+}
