31d268049c8c86c7733ba9bfdcbaaa88367ec524
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-16 14:17:08 -0700

    Create quadratic_solver.cpp

---
+++ chapter_4/exercise_4_18/quadratic_solver.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    double a = 0;
+    double b = 0;
+    double c = 0;
+    
+    
+    std::cout << "Quadratic Formula Solver (ax^2 + bx + c = 0)" << std::endl;
+    
+    std::cout << "Enter a: ";
+    std::cin >> a;
+    
+    std::cout << "Enter b: ";
+    std::cin >> b;
+    
+    std::cout << "Enter c: ";
+    std::cin >> c;
+    
+    
+    
+    return 0;
+}
