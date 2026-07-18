80e271ccf4991e3240ede1b25310bbb1e412beaf
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-17 23:14:11 -0700

    Update names_and_ages.cpp

--- chapter_4/exercise_4_19/names_and_ages.cpp
+++ chapter_4/exercise_4_19/names_and_ages.cpp
@@ -6,6 +6,18 @@ int main()
     std::vector<std::string> names;
     std::vector<int> ages;
     
+    std::string name;
+    int age = 0;
+
+    std::cout << "Enter name and age (e.g Joe 17): ";
+    std::cin >> name >> age;
+
+    while (name != "NoName" && age != 0)
+    {
+        std::cout << "Enter name and age (e.g Joe 17): ";
+        std::cin >> name >> age;
+    }
+    
     
     
     
