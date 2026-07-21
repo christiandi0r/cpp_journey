74ec560e05a4659bc11959a795ae71469b4b317a
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-21 10:41:28 -0700

    Update person.cpp

--- chapter_6/exercise_6_04/person.cpp
+++ chapter_6/exercise_6_04/person.cpp
@@ -1,1 +1,14 @@
+#include "person.hpp"
 #include <iostream>
+
+Person::Person() : _name(" "), _age(0) {}
+
+Person::Person(const std::string& name, int age) : _name(name), _age(age) {}
+
+void Person::setName(const std::string& name) { _name = name; }
+
+std::string Person::getName() const { return _name; }
+
+void Person::setAge(int age) { _age = age; }
+
+int Person::getAge() const { return _age;}
