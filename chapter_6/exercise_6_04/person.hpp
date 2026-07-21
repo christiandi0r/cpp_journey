a56e3e2d3056080f658f33fff4c363b60302c530
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-20 17:42:34 -0700

    Create person.hpp

---
+++ chapter_6/exercise_6_04/person.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Person
+{
+    public:
+    
+        Person(std::string name, int age);
+    
+    
+    private:
+        
+        std::string _name;
+        int _age;
+
+};
