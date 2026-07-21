27edbb2beff486bec74165ed5bafec034850817e
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-21 10:40:37 -0700

    Update person.hpp

--- chapter_6/exercise_6_04/person.hpp
+++ chapter_6/exercise_6_04/person.hpp
@@ -1,18 +1,25 @@
-#pragma once
-#include <iostream>
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
 #include <string>
-#include <vector>
 
 class Person
 {
-    public:
-    
-        Person(std::string name, int age);
-    
-    
     private:
         
         std::string _name;
         int _age;
 
+    public:
+
+        Person();
+        Person(const std::string& name, int age);
+            
+        void setName(const std::string& name);
+        std::string getName() const;
+            
+        void setAge(int age);
+        int getAge() const;
 };
+
+#endif
