5d1f4c8a464e0a32a9df3fb35fc3c9e561c9d2e9
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-22 14:55:03 -0700

    Update print.cpp

--- chapter__8/exercise_8_02/print.cpp
+++ chapter__8/exercise_8_02/print.cpp
@@ -2,9 +2,19 @@
 #include <vector>
 #include <string>
 
+void print(const std::string& label, const std::vector<int>& vec)
+{
+    std::cout << label <<": ";
+    
+    for (const auto& i : vec) { std::cout << i << " "; }
+}
+
 int main()
 {
-    std::cout<<"Hello World";
+    std::vector<int> scores = {12, 4, 5, 87};
+    std::string label = "Scores";
+    
+    print(label, scores);
 
     return 0;
 }
