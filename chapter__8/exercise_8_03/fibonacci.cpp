b501d2929c3f8ffc4cfd74c5900fcb8b6c2e1de7
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-22 15:16:20 -0700

    Create fibonacci.cpp

---
+++ chapter__8/exercise_8_03/fibonacci.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// @param label: the label you want to be printed out
+// @param vec: the container of values you want to print 
+//             out to the screen
+void print(const std::string& label, const std::vector<int>& vec)
+{
+    std::cout << label << ": ";
+    
+    for (const auto& i : vec) { std::cout << i << " "; }
+}
+
+int main()
+{
+    std::vector<int> values;
+    std::string label;
+    
+    std::cout << "Enter your label (e.g. Scores): ";
+    std::cin >> label;
+    
+    std::cout << "\nEnter values (input \'|\' to stop): ";
+    
+    for (int value; std::cin >> value;) { values.push_back(value); }
+    
+    std::cout << "\n"; 
+    
+    print(label, values);
+
+    return 0;
+}
