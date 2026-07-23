52f5f6a98780fec4a09e8f0d5d436fca7a12417a
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-23 14:01:28 -0700

    Update reverse_vectors.cpp

--- chapter_8/exercise_8_05/reverse_vectors.cpp
+++ chapter_8/exercise_8_05/reverse_vectors.cpp
@@ -3,14 +3,20 @@
 
 std::vector<int> reverse_vector(const std::vector<int>& v)
 {
+    std::vector<int> newVec;
     
-
+    for (int i = v.size(); i > 0; i--) { newVec.push_back(i); }
+    
+    return newVec;
 }
-
 
 int main()
 {
+    std::vector<int> nums = {1, 2, 3, 4, 5};
     
+    nums = reverse_vector(nums);
+    
+    for (const auto& i : nums) { std::cout << i << " "; }
 
     return 0;
 }
