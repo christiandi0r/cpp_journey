67b59f72ba74231e78ae91f3b5558c87ffe5f405
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-23 14:43:32 -0700

    Update reverse_int_vectors.cpp

--- chapter_8/exercise_8_05/reverse_int_vectors.cpp
+++ chapter_8/exercise_8_05/reverse_int_vectors.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include <vector>
 
+// ************************************************************
+//
+// @brief: since we pass-by-const-reference, we can only read the
+//         values within the vector and can't modify anything.
+//
+//         1. Create new vector
+//         2. Start for-loop from the back of vector being passed
+//         3. Copy values to new vector
+//
+// ************************************************************
 std::vector<int> reverse_vector(const std::vector<int>& v)
 {
     std::vector<int> newVec;
     
-    for (int i = v.size(); i > 0; i--) { newVec.push_back(i); }
+    for (int i = v.size() - 1; i >= 0; i--) { newVec.push_back(v.at(i)); }
     
     return newVec;
 }
 
+// ************************************************************
+//
+// @brief: since we pass-by-reference, we are directly modifying
+//         the vector we are passing through argument.
+//
+//         1. Check if vector is empty first
+//         2. Compute middle and end of vector
+//         3. Swap values until we hit the middle point of vector
+//
+// ************************************************************
 void reverse_vector_swap(std::vector<int>& vec)
 {
     if (vec.empty()) { return; }
@@ -20,13 +40,13 @@ void reverse_vector_swap(std::vector<int>& vec)
     for (int i = 0; i < middle; i++)
     {
         std::swap(vec.at(i), vec.at(end));
-        end--;
+        end--;  
     }
 }
 
 int main()
 {
-    std::vector<int> nums = {1, 2, 3, 4};
+    std::vector<int> nums = {10, 20, 30, 40};
     
     //nums = reverse_vector(nums);
     
