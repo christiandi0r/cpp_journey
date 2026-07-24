85eae7bae98107574d8cc9f50cbb17b748709533
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-24 12:37:25 -0700

    Update compute_parallel_vector_vals.cpp

--- chapter_8/exercise_8_09/compute_parallel_vector_vals.cpp
+++ chapter_8/exercise_8_09/compute_parallel_vector_vals.cpp
@@ -7,8 +7,19 @@ double compute_groceries(const std::vector<double>& prices, const std::vector<do
 
     if (weights.size() != prices.size()) 
     {  
-        if (weights.size() > prices.size()) { std::cout << "Error: You are missing a price. Try again." << std::endl; }
-        else { std::cout << "Error: You are missing a weight. Try again." << std::endl; }
+        if (weights.size() > prices.size()) 
+        {  
+            std::cout << "Error: You are missing a price. Try again." << std::endl; 
+            
+            return -1;
+        }
+        
+        else 
+        {  
+            std::cout << "Error: You are missing a weight. Try again." << std::endl; 
+            
+            return -1;
+        }
     }
 
     for (int i = 0; i < weights.size(); i++) { total += prices.at(i) * weights.at(i); }
@@ -21,7 +32,7 @@ int main()
     std::vector<double> prices = {3, 3, 3};
     std::vector<double> weights = {1, 1, 1};
 
-    int total = 0;
+    double total = 0;
 
     total = compute_groceries(prices, weights);
 
