917aaa9f210ae0c1ae8e4c4d47bb9465a5be019f
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-28 10:02:28 -0700

    Update analyze_vector_strings.cpp

--- chapter_8/exercise_8_13/analyze_vector_strings.cpp
+++ chapter_8/exercise_8_13/analyze_vector_strings.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 #include <string>
 
-std::vector<int> num_chars_string(const std::vector<std::string>& vec)
+std::vector<int> num_chars_string(const std::vector<std::string>& words)
 {
     std::vector<int> num_chars;
     
+    for (const auto& word : words) { num_chars.push_back(word.size()); }
     
     
+    
+    return num_chars;
 }
-
-
-
-
-
 
 int main()
 {
-    std::cout<<"Hello World";
+    std::vector<std::string> words = {"hey", "there"};
+    
+    num_chars_string(words);
 
     return 0;
 }
