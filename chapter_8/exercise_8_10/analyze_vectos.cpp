8332675a8d90bc2f8b325248ce056dbb7eb3bfb1
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-27 20:03:12 -0700

    Create analyze_vectos.cpp

---
+++ chapter_8/exercise_8_10/analyze_vectos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+T maxv(const std::vector<T>& vec)
+{
+    T max_element;
+    return max_element;
+
+}
+
+
+int main()
+{
+    std::vector<int> nums;
+
+    return 0;
+}
