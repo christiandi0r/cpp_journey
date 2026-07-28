1ff3c13e62b737b06d98e801956c7fb0a3ecd528
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-27 21:36:57 -0700

    Update largest_element_vector.cpp

--- chapter_8/exercise_8_10/largest_element_vector.cpp
+++ chapter_8/exercise_8_10/largest_element_vector.cpp
@@ -4,15 +4,23 @@
 template <typename T>
 T maxv(const std::vector<T>& vec)
 {
-    T max_element;
-    return max_element;
+    T largest_element = vec.at(0);
 
+    for (const auto& element : vec)
+    {
+        if (element > largest_element) { largest_element = element; }
+    }
+
+    return largest_element;
 }
-
 
 int main()
 {
-    std::vector<int> nums;
+    std::vector<int> nums = {3, 69, 2, 5};
+
+    int largest_element = maxv(nums);
+
+    std::cout << "Largest element: " << largest_element << std::endl;
 
     return 0;
 }
