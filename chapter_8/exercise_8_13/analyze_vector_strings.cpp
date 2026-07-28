c6a1f5ddf1fe60b1192acac7c08f431263050b60
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-28 09:55:56 -0700

    Update analyze_vector_strings.cpp

--- chapter_8/exercise_8_13/analyze_vector_strings.cpp
+++ chapter_8/exercise_8_13/analyze_vector_strings.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+std::vector<int> num_chars_string(const std::vector<std::string>& vec)
+{
+    std::vector<int> num_chars;
+    
+    
+    
+}
+
+
+
+
+
 
 int main()
 {
