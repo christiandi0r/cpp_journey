43fbdc6596a34a62cd83b5783aa36416783c5821
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-28 10:25:08 -0700

    Update analyze_vector_strings.cpp

--- chapter_8/exercise_8_13/analyze_vector_strings.cpp
+++ chapter_8/exercise_8_13/analyze_vector_strings.cpp
@@ -11,11 +11,28 @@ std::vector<int> count_num_chars(const std::vector<std::string>& words)
     return numChars;
 }
 
+void longest_and_shortest_string(const std::vector<std::string>& words)
+{
+    std::string longest = words.at(0);
+    std::string shortest = words.at(0);
+    
+    for (int i = 0; i < words.size(); i++)
+    {
+        if (words.at(i).size() > longest.size()) { longest = words.at(i); }
+        if (words.at(i).size() < shortest.size()) { shortest = words.at(i); }
+    }
+    
+    std::cout << "Longest word: " << longest << std::endl;
+    std::cout << "Shortest word: " << shortest << std::endl;
+}
+
 int main()
 {
     std::vector<std::string> words = {"hey", "there"};
     
     std::vector<int> numChars = count_num_chars(words);
+    
+    longest_and_shortest_string(words);
     
     return 0;
 }
