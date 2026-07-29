a8d0557b617868f24c982a5a5d76d8342e768a9a
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-29 14:01:21 -0700

    Update Name_pairs.cpp

--- chapter_9/exercise_9_02/Name_pairs.cpp
+++ chapter_9/exercise_9_02/Name_pairs.cpp
@@ -1,3 +1,38 @@
 #include "Name_pairs.hpp"
 #include <iostream>
+#include <string>
 
+void Name_pairs::read_names()
+{
+    std::cout << "Enter name, input \"quit\" to end program. " << std::endl;
+    
+    std::cout << "\nEnter some names: ";
+    
+    for (std::string name; std::cin >> name;)
+    {
+        if (name == "quit") { break; }
+        
+        names.push_back(name);
+    }
+}
+
+void Name_pairs::read_ages()
+{
+    double age = 0;
+    
+    for (int i = 0; i < names.size(); i++)
+    {
+        std::cout << "Enter " << names.at(i) << "'s " << "age: ";
+        std::cin >> age;
+        
+        ages.push_back(age);
+    }
+}
+
+
+void Name_pairs::print()
+{
+    for (int i = 0; i < names.size(); i++) { std::cout << names.at(i) << " " << ages.at(i) << std::endl; }
+}
+
+//void Name_pairs::sort();
