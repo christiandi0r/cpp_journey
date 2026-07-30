31a5dac3a0213ed1a47ab20af54848592a990498
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-30 10:58:07 -0700

    Update book.hpp

--- chapter_9/exercise_9_05/book.hpp
+++ chapter_9/exercise_9_05/book.hpp
@@ -7,13 +7,15 @@ class Book
 {
     public:
     
-        Book() : _ISBN{"n-n-n-x"}, _title{" "}, _author{" "}, _copyrightDate{"YYYY-JAN-DD"} {}
+        Book() : _ISBN{"n-n-n-x"}, _title{"Book Title"}, _author{"Joe Smith"}, _copyrightDate{"YYYY-JAN-DD"} {}
         
         Book(const std::string& ISBN, 
              const std::string& title, 
              const std::string& author
              const std::string& copyrightDate
              ) : _ISBN{ISBN}, _title{title}, _author{author}, _copyrightDate{copyrightDate} {}
+             
+        
              
     
     
