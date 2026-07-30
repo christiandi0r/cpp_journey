c30fe07f6a65ef3390b316e2b2e90bae5f73dee4
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-30 12:30:59 -0700

    Update book.hpp

--- chapter_9/exercise_9_05/book.hpp
+++ chapter_9/exercise_9_05/book.hpp
@@ -14,10 +14,33 @@ class Book
              const std::string& author
              const std::string& copyrightDate
              ) : _ISBN{ISBN}, _title{title}, _author{author}, _copyrightDate{copyrightDate} {}
-             
         
-             
-    
+        
+        // ISBN setter and getter
+        void set_ISBN(const std::string& ISBN) { _ISBN = ISBN; }
+        std::string get_ISBN() const { return _ISBN; }
+        
+        // Book title getter and setter
+        void set_title(const std::string& title) { _title = title; }
+        std::string get_title() const { return _title; }
+        
+        // Book author getter and setter
+        void set_author(const std::string& author) { _author = author; }
+        std::string get_author() const { return _author; }
+        
+        // Copyright date setter and getter
+        void set_copyright_date(const std::string& copyrightDate) { _copyrightDate = copyrightDate; }
+        std::string get_copyright_date() const { return _copyrightDate; }
+        
+        // Book checkout functions
+        void checkout_book() { _isCheckedOut = true; }
+        void return_book() { _isCheckedOut = false; }
+        
+        // TO-DO: Implement these functions in book.cpp
+        void book_status();
+        void 
+        
+        
     
     private:
     
@@ -25,6 +48,7 @@ class Book
         std::string _title;
         std::string _author;
         std::string _copyrightDate;
+        bool _isCheckedOut;
         
   
 };
