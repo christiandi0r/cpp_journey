20c48273d83834a37d11d5a9a70382238c8a7774
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-31 12:47:34 -0700

    Create book.hpp

---
+++ chapter_9/exercise_9_06/book.hpp
@@ -0,0 +1,66 @@
+#ifndef BOOK_HPP
+#define BOOK_HPP
+
+#include <iostream>
+#include <string>
+
+class Book
+{
+    public:
+        
+        Book() : _title{"Book Title"}, _author{"Book author"}, _copyrightDate{"YYYY-JAN-DD"}, _ISBN{"n-n-n-x"}, _isCheckedOut{false} {}
+        
+        Book(const std::string& title, 
+             const std::string& author,
+             const std::string& copyrightDate,
+             const std::string& ISBN,
+             bool isCheckedOut
+             ) : _title{title}, _author{author}, _copyrightDate{copyrightDate}, _ISBN{ISBN}, _isCheckedOut{isCheckedOut} {}
+        
+        // Book title getter and setter
+        void set_title(const std::string& title) { _title = title; }
+        std::string get_title() const { return _title; }
+        
+        // Book author getter and setter
+        void set_author(const std::string& author) { _author = author; }
+        std::string get_author() const { return _author; }
+        
+        // Copyright date setter and getter
+        void set_copyright_date(const std::string& copyrightDate) { _copyrightDate = copyrightDate; }
+        std::string get_copyright_date() const { return _copyrightDate; }
+
+        // ISBN setter and getter
+        void set_ISBN(const std::string& ISBN) { _ISBN = ISBN; }
+        std::string get_ISBN() const { return _ISBN; }
+        
+        // Book checkout functions
+        void checkout_book() { _isCheckedOut = true; }
+        void return_book() { _isCheckedOut = false; }
+        
+        // Status if book is checked out or available
+        void book_status()
+        {
+            if (_isCheckedOut) { std::cout << "Book is currently checked out." << std::endl; }
+            else { std::cout << "Book is currently available." << std::endl; }
+        }
+        
+        void print_details() 
+        {  
+            std::cout << "Title: " << _title << std::endl;
+            std::cout << "Author: " << _author << std::endl;
+            std::cout << "Copyright Date: " << _copyrightDate << std::endl;
+            std::cout << "ISBN: " << _ISBN << std::endl;
+            std::cout << "Status: ";
+            book_status();
+        }
+
+    private:
+    
+        std::string _title;
+        std::string _author;
+        std::string _copyrightDate;
+        std::string _ISBN;
+        bool _isCheckedOut;
+};
+
+#endif
