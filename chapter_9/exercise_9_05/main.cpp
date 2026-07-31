9d9919ec0683445ca60e23cf1c59e6bd667c28f1
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-31 12:31:09 -0700

    Update main.cpp

--- chapter_9/exercise_9_05/main.cpp
+++ chapter_9/exercise_9_05/main.cpp
@@ -1,12 +1,21 @@
 #include "book.hpp"
 #include <iostream>
+#include <string>
 
 
 int main()
 {
     Book book1;
+
+    Book book2("5-4-3-2-B", "PPP2", "Bjarne Stroutsoup", "2010 JUN 05", false);
     
     book1.print_details();
+
+    std::cout << "\n";
+
+    book2.checkout_book();
+
+    book2.print_details();
     
     return 0;
 }
