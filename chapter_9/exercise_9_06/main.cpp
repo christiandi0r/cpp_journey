e2bcef62633edb977aac88965a38e762f2754210
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-07-31 13:16:27 -0700

    Create main.cpp

---
+++ chapter_9/exercise_9_06/main.cpp
@@ -0,0 +1,17 @@
+#include "book.hpp"
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    Book book1;
+
+    Book book2("PPP2", "Bjarne Stroutsoup", "2010 JUN 05", "5-4-3-x", false);
+    
+    if (book1 == book2) { std::cout << "Books are the same." << std::endl; }
+
+    else { std::cout << "Books are NOT the same." << std::endl; }
+    
+    return 0;
+}
