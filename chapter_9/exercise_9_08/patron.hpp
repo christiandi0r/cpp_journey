e9034a29697359375d8c91675370a5ab5704d021
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-02 20:25:53 -0700

    Update patron.hpp

--- chapter_9/exercise_9_08/patron.hpp
+++ chapter_9/exercise_9_08/patron.hpp
@@ -1,1 +1,25 @@
+#ifndef PATRON_HPP
+#define PATRON_HPP
+
 #include <iostream>
+#include <string>
+
+class Patron 
+{
+    public:
+
+        Patron() : _name{"Unknown"}, _libraryCardNum{0}, _libraryFees{0}, _owesFees{false} {}
+
+
+    private:
+
+        std::string _name;
+        int _libraryCardNum;
+        double _libraryFees;
+        bool _owesFees;
+
+};
+
+
+
+#endif
