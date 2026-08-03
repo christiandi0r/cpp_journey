77a00bd31bc4fe67d0bfada9a44d53d93f04361c
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-03 10:07:00 -0700

    Update patron.hpp

--- chapter_9/exercise_9_08/patron.hpp
+++ chapter_9/exercise_9_08/patron.hpp
@@ -8,20 +8,37 @@ class Patron
 {
     public:
 
-        Patron() : _name{"Unknown"}, _libraryCardNum{0}, _libraryFees{0}, _owesFees{false} {}
+        Patron() : _name{"Unknown"}, _libraryCardNum{0}, _libraryFees{0} {}
 
         Patron(const std::string& name,
                int libraryCardNum,
                double libraryFees,
                bool owesFees
-               ) : _name{name}, _libraryCardNum{libraryCardNum}, _libraryFees{libraryFees}, _owesFees{owesFees} {}
+               ) : _name{name}, _libraryCardNum{libraryCardNum}, _libraryFees{libraryFees} {}
+        
+        // Patron's name setter and getter
+        void setName(const std::string& name) { _name = name; }
+        std::string getName() const { return _name; }
 
+        // Library card number setter and getter
+        void setLibraryCardNum(int libraryCardNum) { _libraryCardNum = libraryCardNum; }
+        int getLibraryCardNum() const { return _libraryCardNum; }
+
+        //Library fees setter and gettter
+        void setLibraryFees(double libraryFees) { _libraryFees = libraryFees; }
+        double getLibraryFees() const { return _libraryFees; }
+        
+        bool doesPatronOweFees()
+        {
+            if (_libraryFees > 0) { std::cout << "Patron owes fees."; return true; }
+            else { std::cout << "Patron does NOT owe fees."; return false; }
+        }
+    
     private:
 
         std::string _name;
         int _libraryCardNum;
         double _libraryFees;
-        bool _owesFees;
 };
 
 #endif
