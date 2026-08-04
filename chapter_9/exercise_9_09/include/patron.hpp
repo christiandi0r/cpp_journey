4017a0cfb68232c3923efb713f826e54a60f6fb7
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-03 19:48:03 -0700

    Create patron.hpp

---
+++ chapter_9/exercise_9_09/include/patron.hpp
@@ -0,0 +1,44 @@
+#ifndef PATRON_HPP
+#define PATRON_HPP
+
+#include <iostream>
+#include <string>
+
+class Patron 
+{
+    public:
+
+        Patron() : _name{"Unknown"}, _libraryCardNum{0}, _libraryFees{0} {}
+
+        Patron(const std::string& name,
+               int libraryCardNum,
+               double libraryFees,
+               bool owesFees
+               ) : _name{name}, _libraryCardNum{libraryCardNum}, _libraryFees{libraryFees} {}
+        
+        // Patron's name setter and getter
+        void setName(const std::string& name) { _name = name; }
+        std::string getName() const { return _name; }
+
+        // Library card number setter and getter
+        void setLibraryCardNum(int libraryCardNum) { _libraryCardNum = libraryCardNum; }
+        int getLibraryCardNum() const { return _libraryCardNum; }
+
+        //Library fees setter and gettter
+        void setLibraryFees(double libraryFees) { _libraryFees = libraryFees; }
+        double getLibraryFees() const { return _libraryFees; }
+        
+        bool doesPatronOweFees() const
+        {
+            if (_libraryFees > 0) { std::cout << "Patron owes fees."; return true; }
+            else { std::cout << "Patron does NOT owe fees."; return false; }
+        }
+    
+    private:
+
+        std::string _name;
+        int _libraryCardNum;
+        double _libraryFees;
+};
+
+#endif
