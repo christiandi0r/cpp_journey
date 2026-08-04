940c08219413fa558f87ba1f807e3797ff6a2115
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-04 09:31:23 -0700

    Update main.cpp

--- chapter_9/exercise_9_09/source/main.cpp
+++ chapter_9/exercise_9_09/source/main.cpp
@@ -1,11 +1,24 @@
 #include "library.hpp"
 
 #include <iostream>
+#include <vector>
 #include <string>
+
+
 
 int main()
 {
+    Library huntington;
     
-
+    Book book1;
+    Book book2("PPP2", "Bjarne Stroutsoup", "2010 JUN 05", "5-4-3-x", Book::Genre::NonFiction, false);
+    
+    huntington.add_book(book1);
+    huntington.add_book(book2);
+    
+    huntington.checkout_book_from_lib(book2);
+    
+    huntington.print_books();
+    
     return 0;
 }
