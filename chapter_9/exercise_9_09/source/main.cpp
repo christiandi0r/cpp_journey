f2c390730afc4e001a36c7f8372a54d877c8daff
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-04 14:10:43 -0700

    Update main.cpp

--- chapter_9/exercise_9_09/source/main.cpp
+++ chapter_9/exercise_9_09/source/main.cpp
@@ -11,23 +11,26 @@ int main()
     // Demo book functions
     Book book1;
     Book book2("PPP2", "Bjarne Stroutsoup", "2010 JUN 05", "5-4-3-x", Book::Genre::NonFiction, false);
+    Book book3;
     
     huntington.add_book(book1);
     huntington.add_book(book2);
     
-    huntington.checkout_book_from_lib(book2);
-    
-    //huntington.print_books();
-    
     // Demo patron functions
     Patron patron1("Christian", 98510345, 0.00);
     Patron patron2("Mike", 96578022, 10.50);
+    Patron patron3;
     
     huntington.add_patron(patron1);
     huntington.add_patron(patron2);
     
-    huntington.print_patrons();
+    //huntington.checkout_book_from_lib(book2, patron1);
     
+    huntington.checkout_book_from_lib(book2, patron3);
+    
+    //huntington.print_patrons();
+    //huntington.print_books();
+    //huntington.print_transactions();
     
     return 0;
 }
