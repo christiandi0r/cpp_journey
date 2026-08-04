e848d7bf355f5b6f5689091de568ef7f6c5b15c9
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-04 14:11:08 -0700

    Update library.hpp

--- chapter_9/exercise_9_09/include/library.hpp
+++ chapter_9/exercise_9_09/include/library.hpp
@@ -17,18 +17,44 @@ class Library
             Book book; 
             Patron patron;  
         };
+        
+        friend std::ostream& operator<<(std::ostream& os, const Transaction& transaction)
+        {
+            os << "Name: " << transaction.patron.getName() << "\n";
+            os << "Book: " << transaction.book.get_title() << "\n";
+            
+            return os;
+        }
 
         void add_book(const Book& book) { books.push_back(book); }
         
         void add_patron(const Patron& patron) { patrons.push_back(patron); }
         
-        void checkout_book_from_lib(Book& book)
+        void checkout_book_from_lib(Book& book, Patron& patron)
         { 
-            for (auto& i : books) { if (i == book) i.checkout_book(); }
+            if (patron.getName() == "Unknown") { std::cout << "Error: Person does NOT have library membership." << std::endl; }
+            
+            for (auto& i : books) 
+            { 
+                if (i.book_status() == "Book is NOT currently available.") { std::cout << "Error: " << book.get_title() << " is NOT available." << std::endl; }
+                
+                if (i == book) 
+                { 
+                    i.checkout_book();
+                    
+                    Transaction transaction;
+                    
+                    transaction.book = book;
+                    transaction.patron = patron;
+                    
+                    transactions.push_back(transaction);
+                }
+            }
         }
         
         void print_books() { for (const auto& book : books) std::cout << book << std::endl; }
         void print_patrons() { for (const auto& patron : patrons) std::cout << patron << std::endl; }
+        void print_transactions() { for (const auto& transaction : transactions) std::cout << transaction << std::endl; }
         
     private:
 
