bad229b5450942bd40a295efdc1d8a46f962794d
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-04 09:37:00 -0700

    Update main.cpp

--- chapter_9/exercise_9_09/source/main.cpp
+++ chapter_9/exercise_9_09/source/main.cpp
@@ -10,6 +10,7 @@ int main()
 {
     Library huntington;
     
+    // Demo book functions
     Book book1;
     Book book2("PPP2", "Bjarne Stroutsoup", "2010 JUN 05", "5-4-3-x", Book::Genre::NonFiction, false);
     
@@ -18,7 +19,13 @@ int main()
     
     huntington.checkout_book_from_lib(book2);
     
-    huntington.print_books();
+    //huntington.print_books();
+    
+    // Demo patron functions
+    Patron patron1("Christian", 98510345, 0.00, false);
+    
+    huntington.add_patron(patron1);
+    
     
     return 0;
 }
