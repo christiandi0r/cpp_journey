d3fb3aef5709f211a3b97b198c1726ff8cc85450
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-06 21:08:41 -0700

    Create part_time_employee.hpp

---
+++ chapter_9/employee_virtual_functions/part_time_employee.hpp
@@ -0,0 +1,39 @@
+#ifndef PART_TIME_EMPLOYEE_HPP
+#define PART_TIME_EMPLOYEE_HPP
+
+#include "employee.hpp"
+
+#include <string>
+
+class PartTimeEmployee : public Employee
+{
+    public:
+
+        PartTimeEmployee() : _hourlyWage{0}, _monthlyHours{0} {}
+
+        PartTimeEmployee(const std::string& name,
+                         double hourlyWage,
+                         double monthlyHours
+                         ) : _name{name}, _hourlyWage{hourlyWage}, _monthlyHours{monthlyHours} {}
+
+        void setName(const std::string& name) { _name = name; }
+        std::string getName() const { return _name; }
+        
+        // Part-time employee hourly wage functions
+        void setHourlyWage(double hourlyWage) { _hourlyWage = hourlyWage; }
+        double getHourlyWage() const { return _hourlyWage; }
+        
+        // Part-time employee monthly hours worked functions
+        void setMonthlyHours(double monthlyHours) { _monthlyHours = monthlyHours; }
+        double getHourlyWage() const { return _monthlyHours; }
+
+        double calculatePay() const override { return _hourlyWage * _monthlyHours; }
+
+    private:
+
+        std::string _name;
+        double _hourlyWage;
+        double _monthlyHours;
+};
+
+#endif
