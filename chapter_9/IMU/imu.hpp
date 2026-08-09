17e0a813f83ec6800ebe69dce6275b3984a82d70
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-08 23:07:59 -0700

    Update imu.hpp

--- chapter_9/IMU/imu.hpp
+++ chapter_9/IMU/imu.hpp
@@ -10,6 +10,12 @@ class IMU
 
     private:
 
+        double _ax, _ay, _az;
+        double _wx, _wy, _wz;
+
+        bool _isInitialized;
+        bool _isCalibrated;
+        bool _isSensorHealthy;
 
 
 };
