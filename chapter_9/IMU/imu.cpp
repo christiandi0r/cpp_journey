f45724b1d654c51bddc2fce241ecc125cdf12bff
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-11 20:42:14 -0700

    Update imu.cpp

--- chapter_9/IMU/imu.cpp
+++ chapter_9/IMU/imu.cpp
@@ -1,6 +1,9 @@
 #include "imu.hpp"
 
-void IMU::initialize() { if (_isSensorHealthy) _isInitialized = true; }
+void IMU::initialize() 
+{ 
+    if (_isSensorHealthy) { _isInitialized = true; }
+}
 
 void IMU::calibrate()  
 {
