b3452a80411f4180d8c41ee05bb92acccdb33abf
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-20 18:25:47 -0700

    Create main.cpp

---
+++ chapter_18/exercise_18_05/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+std::string cat_dot(const std::string& s1, const std::string& s2)
+{
+    std::string s3 = ".";
+    std::string s4;
+
+    s4 = s1 + s3 + s2;
+
+    return s4;
+}
+
+int main()
+{
+    std::string s1 = "Niels";
+    std::string s2 = "Bohr";
+
+    std::cout << cat_dot(s1, s2) << std::endl;
+
+    return 0;
+}
