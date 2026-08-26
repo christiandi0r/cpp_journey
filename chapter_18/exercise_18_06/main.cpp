7da37733d2fa0f8225defcd100ab5ba4107d3b23
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-25 20:46:31 -0700

    Update main.cpp

--- chapter_18/exercise_18_06/main.cpp
+++ chapter_18/exercise_18_06/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
 
+std::string cat_dot(const std::string& s1, 
+                    const std::string& s2, 
+                    const std::string& s3) { return s1 + s3 + s2; }
+
+int main()
+{
+    std::string s1 = "Niels";
+    std::string s2 = "Bohr";
+    std::string s3;
+
+    std::cout << "Input a string to separate two other strings" << std::endl;
+    std::cout << "Enter: ";
+    std::cin >> s3;
+
+    std::cout << cat_dot(s1, s2, s3);
+
+    return 0;
+}
